5aae6477dd62633e6ff5cb8683ba6ed8010acb66
themasterlag <parraducuarajuanesteban@gmail.com>
2023-03-06 13:17:17 -0500

    Generacion de pdf base para bancolombia

--- src/app/pagos/pagos.component.ts
+++ src/app/pagos/pagos.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GeneralesService } from 'app/services/generales.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Loading, Report } from 'notiflix';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+declare var require: any;
+const htmlToPdfmake = require("html-to-pdfmake");
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 export interface PeriodicElement {
   Check: boolean;
@@ -18,7 +23,7 @@ export interface PeriodicElement {
   templateUrl: './pagos.component.html',
   styleUrls: ['./pagos.component.css']
 })
-export class PagosComponent implements OnInit {
+export class PagosComponent implements OnInit  {
   panelOpenState = false;
   preliquidacion:any
   consulta:boolean = false
@@ -36,6 +41,7 @@ export class PagosComponent implements OnInit {
   responsableTabla: PeriodicElement[] = [];
   dataSource:MatTableDataSource<PeriodicElement> = new MatTableDataSource<PeriodicElement>();
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild('pdfTable') pdfTable: ElementRef;
 
   constructor(private servicio:GeneralesService) { }
 
@@ -149,7 +155,14 @@ this.validaciondatos()
           // this.dataSource.splice(i, 1);
           // this.listarResponsable.splice(i, 1);
         }
-
+  
+  generarPdfBancolombia(){
+    console.log(this.pdfTable);
+    const pdfTable = this.pdfTable.nativeElement;
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).download(); 
+  }
 }
 
 
