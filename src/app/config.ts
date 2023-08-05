117dd3ae869a6af4ba4f7dcbac41339d3f760d52
themasterlag <parraducuarajuanesteban@gmail.com>
2023-08-05 09:58:05 -0500

    Ajuste de ambiente

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,30 +1,12 @@
-/*=============================================
-Exportamos la ruta para tomar imágenes
-=============================================*/
-export let Path = {
+import { environment } from "environments/environment";
 
-    //url: 'http://localhost:4200/assets/'
-     //url: 'http://54.174.81.71/administramos_software/assets/'
-        url: 'https://resultadoslaboratorios.com/assets/'
-      }
-      
-      /*=============================================
+/*=============================================
       Exportamos el endPoint de la APIREST de Firebase
       =============================================*/
-      export let Api = {
-      
-      
-    // url: 'http://localhost/api_administramos/'
+export let Api = {
+  // url: 'http://localhost/api_administramos/'
   // url: 'http://54.174.81.71/administramos_software/api_administramos/'
-    //  url: 'http://10.250.9.36:3000/api/arriendos/'
-        url: 'http://localhost:3000/api/arriendos/'
-        // url: 'http://10.0.102.128:3000/api/arriendos/'
-      }
-      
-      export let ApiMail = {
-      
-        //	url: 'https://mp-prueba-2f699.firebaseio.com/' //YOUR FIREBASE ENDPOINT
-         //url: 'http://localhost/apimarket_angular-php/'
-            url: 'https://femseapto.com.co/api_mail/'
-        }
-      
+  //  url: 'http://10.250.9.36:3000/api/arriendos/'
+  url: environment.url,
+  // url: 'http://10.0.102.128:3000/api/arriendos/'
+};
